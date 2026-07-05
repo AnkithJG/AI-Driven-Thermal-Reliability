5157694b2a193484ff91ebb97f271bd000f6cab0
Ankith Ganji <ganjiankith@gmail.com>
2026-07-05 13:01:59 -0500

    forward defined for spectralConv2d

--- fno_model.py
+++ fno_model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class SpectralConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, modes):
@@ -13,5 +14,22 @@ class SpectralConv2d(nn.Module):
         self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, modes, modes, dtype=torch.cfloat))
         self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, modes, modes, dtype=torch.cfloat))
 
+    def compl_mul2d(self, input, weights):
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
+
     def forward(self, x):
-        pass
+        batchsize = x.shape[0]
+        x_ft = torch.fft.rfft(x)
+        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes, :self.modes] = self.compl_mul2d(x_ft[:, :, :self.modes, :self.modes], self.weights1)
+        out_ft[:, :, -self.modes:, :self.modes] = self.compl_mul2d(x_ft[:, :, -self.modes:, :self.modes], self.weights2)
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+        return x
+    
+
+
+class FNO2d(nn.Module):
+    def __init__(self, modes, width):
+        super(FNO2d, self).__init__()
+        self.modes = modes
+        self.width = width
